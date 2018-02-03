48ed73996b97b242679821deccaf1cb25214a60d
HariS <HariS@Hari.gateway>
2018-02-04 07:16:53 +1100

    Readme.md added

--- main.js
+++ main.js
@@ -98,3 +98,22 @@ console.log(action.next());
 console.log(action.next());
 console.log(action.next());
 console.log(action.next());
+
+
+function* eachItem(arr) {
+  for(var i=0; i<arr.length; i++) {
+    yield arr[i];
+  }
+}
+
+var letters = eachItem(['a','b','c','d','e','f','g']);
+
+var abcs = setInterval(function(){
+  var letter = letters.next();
+  if(letter.done) {
+    clearInterval(abcs);
+    console.log('Now i know my ABCs');
+  } else {
+    console.log(letter.value);
+  }
+  }, 500);
